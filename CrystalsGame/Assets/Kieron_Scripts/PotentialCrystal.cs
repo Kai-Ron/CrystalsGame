59c4d836495c8f24ef87dcc3eaaae04174ec77a9
Kai-Ron <c22470392@mytudublin.ie>
2024-05-15 20:33:08 +0100

    Assembling basic level requirements

--- CrystalsGame/Assets/Kieron_Scripts/PotentialCrystal.cs
+++ CrystalsGame/Assets/Kieron_Scripts/PotentialCrystal.cs
@@ -8,17 +8,29 @@ public class PotentialCrystal : MonoBehaviour
     public bool powered = false;
     public Energy energy;
     public Rigidbody2D rb;
+    public GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
         energy = gameObject.GetComponent<Energy>();
         rb = gameObject.GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(energy.grabbed)
+        {
+            rb = player.GetComponent<Rigidbody2D>();
+            Debug.Log(rb.gameObject);
+        }
+        else
+        {
+            rb = gameObject.GetComponent<Rigidbody2D>();
+        }
+
         if(energy.source)
         {
             energy.source = null;
@@ -27,16 +39,9 @@ public class PotentialCrystal : MonoBehaviour
 
     void FixedUpdate()
     {
-        if(rb.velocity.magnitude > 0.2f && power < rb.velocity.magnitude)
+        if(rb.velocity.magnitude > 0.2f /*&& power < rb.velocity.magnitude*/)
         {
-            if(power < 0)
-            {
-                power = 0.0f;
-            }
-            else
-            {
-                power += 0.02f;
-            }
+            power += 0.02f;
         }
         else
         {
